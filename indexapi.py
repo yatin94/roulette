9f9e0549dbbb29ead868ec12797cceb18b0634e4
Yatin Pandit <yatin@localhost.localdomain>
2020-03-31 19:09:26 +0530

    frontend and backend connection

--- indexapi.py
+++ indexapi.py
@@ -2,7 +2,7 @@ from flask import Flask, flash, redirect, render_template, request,session, abor
 import json
 import os
 
-app =Flask(__name__)
+app =Flask(__name__,static_folder='templates/')
 
 @app.route("/")
 def Index():
@@ -16,7 +16,18 @@ def register():
         Password = request.form['Password']
         print(Name,email,Password)
 
-
+@app.route("/login",methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        Username = request.form['Username']
+        Password = request.form['Password']
+        if Username == 'test' and Password == 'test':
+            return render_template("index.html")
+        flash("Incorrect Password Or Username")
+        return redirect('/login')
+    elif request.method == 'GET':
+        return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run(debug=True,host='192.168.1.33',port="5060")
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='192.168.1.36',port="5060")
