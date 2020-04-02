274256dcd5febc832cdb89bf1af73365773d6640
Yatin Pandit <yatin@localhost.localdomain>
2020-04-03 02:01:56 +0530

    login connectivity

--- indexapi.py
+++ indexapi.py
@@ -1,8 +1,29 @@
 from flask import Flask, flash, redirect, render_template, request,session, abort, jsonify
 import json
 import os
+import mysql.connector 
 
 app =Flask(__name__,static_folder='templates/')
+
+def mysqlconne_all(query,option):
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="roulette"
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute(query)
+        if option == 'select':
+            myresult = mycursor.fetchall()
+            return myresult
+        else:
+            mydb.commit()
+
+def betplace():
+    mysqlconne_all("update testing set Bet ='"+session['bet']+"' where username = '"+session['username']+"'",'update')
+    return None
+
 
 @app.route("/")
 def Index():
@@ -16,18 +37,41 @@ def register():
         Password = request.form['Password']
         print(Name,email,Password)
 
+@app.route("/home")
+def home():
+    if request.method == 'GET':
+        myresult = mysqlconne_all("select name,money,won from testing",'select')
+        return render_template("index.html",result = myresult)
+
+
 @app.route("/login",methods=['POST','GET'])
 def login():
     if request.method == 'POST':
+
         Username = request.form['Username']
         Password = request.form['Password']
-        if Username == 'test' and Password == 'test':
-            return render_template("index.html")
+        try:
+            myresult = mysqlconne_all("SELECT money,username,password FROM testing where username = '" +str(Username)+"'",'select')
+        except Exception as e:
+            flash("Incorrect Password Or Username")
+            print('error'+str(e))
+            return redirect('/login')
+        if Username == myresult[0][1] and Password == myresult[0][2]:
+            session['username'] = myresult[0][1]
+            session['loggedin'] = True
+            session['money'] = myresult[0][0]
+            return redirect("/home")
         flash("Incorrect Password Or Username")
         return redirect('/login')
     elif request.method == 'GET':
-        return render_template('login.html')
+        try:
+            if session['loggedin']:
+                return redirect("/home")
+            else:
+                return render_template('login.html')
+        except:
+            return render_template('login.html')
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='192.168.1.36',port="5060")
+    app.run(debug=True,host='127.0.0.1',port="5060")
