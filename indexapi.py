1be94b8d7d63f47fc8549a00faded16828209ab3
Yatin Pandit <yatin@localhost.localdomain>
2020-04-14 01:59:35 +0530

    bugs

--- indexapi.py
+++ indexapi.py
@@ -2,10 +2,12 @@ from flask import Flask, flash, redirect, render_template, request,session, abor
 import json
 import os
 import mysql.connector 
+import random
 from jinja2 import Environment
+import time
+import threading
 
 app =Flask(__name__,static_folder='templates/')
-
 def mysqlconne_all(query,option):
         mydb = mysql.connector.connect(
         host="localhost",
@@ -37,12 +39,16 @@ def register():
 @app.route("/home")
 def home():
     if request.method == 'GET':
-        myresult = mysqlconne_all("select name,money,won from testing",'select')
-        return render_template("index.html",result = myresult)
+        try:
+            session['loggedin']
+            myresult = mysqlconne_all("select name,money,won from testing",'select')
+            return render_template("index.html",result = myresult)
+        except:
+            return redirect('/login')
 
-@app.route("/betamount/<amt>")
-def betamount(amt):
-    mysqlconne_all("update testing set Bet ='"+amt+"' where username = '"+str(session['username'])+"'","update")
+@app.route("/betamount/<amt>,<number>")
+def betamount(amt,number):
+    mysqlconne_all("update testing set Bet ='"+amt+"', number = '"+str(number)+"',money=money-"+str(amt)+" where username = '"+str(session['username'])+"'","update")
     return amt+" points Booked"
 
 @app.route("/login",methods=['POST','GET'])
@@ -73,9 +79,43 @@ def login():
         except:
             return render_template('login.html')
 
+@app.route("/getrand")
+def getrand():
+    number = mysqlconne_all("select number from value where id = 1","select")
+    return str(number[0][0])
+
+def recurfunc():
+    mysqlconne_all("update value set number ='"+str(random.randint(0,35))+"' where id = '1'","update")
+    time.sleep(15)
+    recurfunc()
+
+@app.route("/winner")
+def winner():
+    number = mysqlconne_all("select number from value where id = 1","select")
+    winner = mysqlconne_all("select Bet,number,money from testing where username = '"+session['username']+"'","select")
+    if winner[0][0] == '' or winner[0][1] == '':
+        return jsonify({'money':winner[0][2],'data':"Did not receive your bet"})
+
+    if int(number[0][0]) == int(winner[0][1]):
+        win_amount = int(winner[0][0])*2
+        mysqlconne_all("update testing set money = money+"+str(win_amount)+",BET='',number='' where username = '"+session['username']+"'","update")
+        return jsonify({'money':winner[0][2],'data':"You Won "+str(win_amount)})
+    else:
+        mysqlconne_all("update testing set BET='',number='' where username = '"+session['username']+"'","update")
+        return jsonify({'money':winner[0][2],'data':"You Lost "+str(winner[0][0])})
+
+
+@app.route("/getmoneytable")
+def getmoneytable():
+    myresult = mysqlconne_all("select name,money,won from testing",'select')
+    return jsonify({"data":myresult,"count":len(myresult)})
+
+
 
 
 if __name__ == "__main__":
+    # t1 = threading.Thread(target=recurfunc)
+    # t1.start()
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='192.168.1.42',port="5060")
   
--- templates/js/custom.js
+++ templates/js/custom.js
@@ -10,14 +10,39 @@
 var red = [32,19,21,25,34,27,36,30,23,5,16,1,14,9,18,7,12,3];
 
 $reset.hide();
-
+test = ""
 $mask.text(maskDefault);
-
+ $(document).ready(function(){
+    setInterval(function(){ 
+   $('#reset').click();
+   $('#spin').click();
+},35000);
+});  
 $spin.on('click',function(){
-  
-  // get a random number between 0 and 36 and apply it to the nth-child selector
- var  randomNumber = Math.floor(Math.random() * 36),
+    var xamt = new XMLHttpRequest();
+    xamt.onreadystatechange = function() {
+      if (this.readyState==4 && this.status == 200) {
+        // return Number(xamt.responseText)
+        
+        var  randomNumber = Number(xamt.responseText),
       color = null;
+      testing(randomNumber)
+      }
+    }
+
+    xamt.open('GET',"http://192.168.1.42:5060/getrand")
+    xamt.send()
+  });
+  
+  
+function testing(randomNumber){
+  // get a random number between 0 and 36 and apply it to the nth-child selector
+
+  
+
+
+//  var  randomNumber = numberdata,
+//       color = null;
       $inner.attr('data-spinto', randomNumber).find('li:nth-child('+ randomNumber +') input').prop('checked','checked');
       // prevent repeated clicks on the spin button by hiding it
        $(this).hide();
@@ -52,12 +77,67 @@ $spin.on('click',function(){
     
     $thisResult = '<li class="previous-result color-'+ color +'"><span class="previous-number">'+ randomNumber +'</span><span class="previous-color">'+ color +'</span></li>';
      
+
+
     $('.previous-list').prepend($thisResult);
    
     
+      var xamt = new XMLHttpRequest();
+      xamt.onreadystatechange = function() {
+        if (this.readyState==4 && this.status == 200) {
+            returnobj = JSON.parse(xamt.responseText)
+            document.getElementById("messagesuccessdiv").style.visibility = 'Visible' 
+            document.getElementById("messagesuccess").innerHTML=returnobj.data
+            document.getElementById("amouth2").innerHTML="Your Total Amount: $"+returnobj.money
+            
+            
+        }
+      }
+
+      xamt.open('GET',"http://192.168.1.42:5060/winner")
+      xamt.send()
+
+
+
+
+
+
+      var tablewinner = new XMLHttpRequest();
+      tablewinner.onreadystatechange = function() {
+        if (this.readyState==4 && this.status == 200) {
+          var a = tablewinner.responseText
+          obj = JSON.parse(a);
+          var tableRef = document.getElementById('mytable').getElementsByTagName('tbody')[0];
+          var i;
+          for (i = 1; i < obj.count+1; i++) {
+            document.getElementById("mytable").deleteRow(1);
+          }
+          obj.data.forEach(element => {
+            var newRow   = tableRef.insertRow();
+            var cellname  = newRow.insertCell(0);
+            var cellmoney  = newRow.insertCell(1);
+            var celltotal  = newRow.insertCell(2);
+            var nametext  = document.createTextNode(element[0]);
+            var moneytext  = document.createTextNode(element[1]);
+            var newText  = document.createTextNode(element[2]);
+            cellname.appendChild(nametext);
+            cellmoney.appendChild(moneytext);
+            celltotal.appendChild(newText);
+          }); 
+
+
+        }
+      }
+
+      tablewinner.open('GET',"http://192.168.1.42:5060/getmoneytable")
+      tablewinner.send()
+    
+
+
+    
     }, timer);
   
-});
+}
 
 
 $reset.on('click',function(){
@@ -90,7 +170,7 @@ function toggleSignup(){
     document.getElementById("login-form").style.display="none";
     document.getElementById("signup-form").style.display="block";
 }
-
+ 
 function toggleLogin(){
     document.getElementById("login-toggle").style.backgroundColor="#8a0000";
     document.getElementById("login-toggle").style.color="#fff";
@@ -99,3 +179,8 @@ function toggleLogin(){
     document.getElementById("signup-form").style.display="none";
     document.getElementById("login-form").style.display="block";
 }
+
+
+
+
+
